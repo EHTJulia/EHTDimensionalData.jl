3fe3dfce7d01e9972b6dd5df90ceb71c88199f93
Documenter.jl <documenter@juliadocs.github.io>
2023-03-08 07:26:10 +0000

    build based on 3a9fb57

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = EHTDimensionalData","category":"page"},{"location":"#EHTDimensionalData","page":"Home","title":"EHTDimensionalData","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for EHTDimensionalData.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [EHTDimensionalData]","category":"page"}]
+}
